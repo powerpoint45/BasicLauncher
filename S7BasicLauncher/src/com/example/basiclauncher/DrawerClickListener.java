85ab427dd0740b60e1f02462f81efc2b9e1ea415
powerpoint45 <powerpointgamez@yahoo.com>
2014-01-02 20:52:41 -0600

    seventh

---
+++ S7BasicLauncher/src/com/example/basiclauncher/DrawerClickListener.java
@@ -0,0 +1,30 @@
+package com.example.basiclauncher;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class DrawerClickListener implements OnItemClickListener {
+
+	Context mContext;
+	MainActivity.Pac[] pacsForAdapter;
+	PackageManager pmForListener;
+	
+	public DrawerClickListener(Context c, MainActivity.Pac[] pacs, PackageManager pm){
+		mContext=c;
+		pacsForAdapter=pacs;
+		pmForListener = pm;
+	}
+	
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int pos, long arg3) {
+		if (MainActivity.appLaunchable){
+			Intent launchIntent = pmForListener.getLaunchIntentForPackage(pacsForAdapter[pos].name);
+			mContext.startActivity(launchIntent);
+		}
+	}
+
+}
