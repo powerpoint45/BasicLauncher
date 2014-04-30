827c51c3cee31b5963c42284c7fc8e7992fe0047
michael <michael@antergos.gateway.2wire.net>
2014-04-29 20:52:48 -0500

    added part 9

---
+++ S9BasicLauncher/src/com/example/basiclauncher/DrawerAdapter.java
@@ -0,0 +1,68 @@
+package com.example.basiclauncher;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DrawerAdapter extends BaseAdapter{
+	Context mContext;
+	MainActivity.Pac[] pacsForAdapter;
+	
+	public DrawerAdapter (Context c, MainActivity.Pac pacs[]){
+		mContext =c;
+		pacsForAdapter = pacs;
+	}
+	
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return pacsForAdapter.length;
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	static class ViewHolder{
+		TextView text;
+		ImageView icon;
+	}
+	@Override
+	public View getView(int pos, View convertView, ViewGroup arg2) {
+		// TODO Auto-generated method stub
+		ViewHolder viewHolder;
+		LayoutInflater li = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		
+		if (convertView==null){
+			convertView = li.inflate(R.layout.drawer_item, null);
+			
+			viewHolder = new ViewHolder();
+			viewHolder.text= (TextView)convertView.findViewById(R.id.icon_text);
+			viewHolder.icon= (ImageView)convertView.findViewById(R.id.icon_image);
+			
+			convertView.setTag(viewHolder);
+		}
+		else
+			viewHolder = (ViewHolder) convertView.getTag();
+		
+		System.out.println("DSSSSSSSSSSSSSSSSSSSSSSS");
+		viewHolder.text.setText(pacsForAdapter[pos].label);
+		viewHolder.icon.setImageDrawable(pacsForAdapter[pos].icon);
+
+		return convertView;
+	}
+
+}
---
+++ S9BasicLauncher/src/com/example/basiclauncher/DrawerLongClickListener.java
@@ -0,0 +1,57 @@
+package com.example.basiclauncher;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+import android.widget.SlidingDrawer;
+import android.widget.TextView;
+
+public class DrawerLongClickListener implements OnItemLongClickListener {
+	
+	Context mContext;
+	SlidingDrawer drawerForAdapter;
+	RelativeLayout homeViewForAdapter;
+	MainActivity.Pac[] pacsForListener;
+	
+	public DrawerLongClickListener(Context ctxt, SlidingDrawer slidingDrawer, RelativeLayout homeView,MainActivity.Pac[] pacs ){
+		mContext = ctxt;
+		drawerForAdapter = slidingDrawer;
+		homeViewForAdapter =homeView;
+		pacsForListener = pacs;
+	}
+	
+	@Override
+	public boolean onItemLongClick(AdapterView<?> arg0, View item, int pos,
+			long arg3) {
+		MainActivity.appLaunchable=false;
+		LayoutParams lp = new LayoutParams(item.getWidth(),item.getHeight());
+		lp.leftMargin = (int) item.getX();
+		lp.topMargin = (int) item.getY();
+		
+		LayoutInflater li = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		LinearLayout ll = (LinearLayout) li.inflate(R.layout.drawer_item, null);
+		
+		((ImageView)ll.findViewById(R.id.icon_image)).setImageDrawable(((ImageView)item.findViewById(R.id.icon_image)).getDrawable());
+		((TextView)ll.findViewById(R.id.icon_text)).setText(((TextView)item.findViewById(R.id.icon_text)).getText());
+		
+		//ll.setOnTouchListener(new AppTouchListener(item.getWidth()));
+		ll.setOnClickListener(new AppClickListener(pacsForListener,mContext));
+		String [] data = new String[2];
+		data[0]=pacsForListener[pos].packageName;
+		data[1]=pacsForListener[pos].name;
+		
+		ll.setTag(data);
+		
+		homeViewForAdapter.addView(ll, lp);
+		drawerForAdapter.animateClose();
+		drawerForAdapter.bringToFront();
+		return false;
+	}
+
+}
